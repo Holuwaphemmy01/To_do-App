12b929d1a82f27c6542794ba52dd5b6d2889c7cf
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2024-12-13 05:38:21 +0100

    Reinitialize repository and add all files

---
+++ backend/To_do_App_Backend/src/main/java/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package app.repository;
+
+import app.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUsername(String username);
+}
