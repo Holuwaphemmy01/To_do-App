a8c0c906c84a521bec622a66b16ac76c0fbca64f
Jacob Oluwafemi <jacoboluwafemi2002@gmail.com>
2024-12-13 06:27:15 +0100

    create task functionality

---
+++ backend/To_do_App_Backend/src/main/java/app/model/Task.java
@@ -0,0 +1,21 @@
+package app.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document
+public class Task {
+    @DBRef
+    private User userId;
+    @Id
+    private String taskId;
+    private String title;
+    private String description;
+    private boolean completed;
+    private Date dueDate;
+}
